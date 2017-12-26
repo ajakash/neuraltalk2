b427ad3cb1b5295f62d339c2079e9e032f0ece2c
ajakash <ajakash91@gmail.com>
2017-12-26 10:50:54 -0800

    finished sets/preprocess_sets.py

--- sets/preprocess_sets.py
+++ sets/preprocess_sets.py
@@ -4,9 +4,10 @@ This code preprocesses captions from a CSV file, and saves it as a json.
 json file will have the form (a list of dictionaries)
 [{ file_loc: 'path/to/image/set/',
     file_names: ['1.jpg', '2.jpg', ...],
-    caption: ['sentence_1', 'sentence_2', ...],
+    captions: ['sentence_1', 'sentence_2', ...],
     id: <create an id here for now?> },  ...]
 
+TO UPDATE: Modify so that it processes folders with <10 images as well
 """
 
 import os
@@ -25,9 +26,29 @@ with open('Category Image Description - Sheet1.csv', 'rb') as csvfile:
         if row[1] == 'Description' or row[1] == 'Empty':
             continue
 
-        print row
+        #print row
 
         filenames = [x[0].split('/')[-1] for x in data_paths]
         loc = filenames.index(row[0])
-        print loc
-        print data_paths[loc]
+        #print loc
+        #print data_paths[loc]
+
+        # Currently taking
+        if len(data_paths[loc][2]) == 10:
+            set_info = {}
+            set_info['file_loc'] = data_paths[loc][0]
+            set_info['file_names'] = data_paths[loc][2]
+
+            caption = row[1].split('.')
+            if caption[-1]=='':
+                caption = caption[:-1]
+
+            set_info['captions'] = caption
+            set_info['id'] = loc
+
+            #print set_info
+            out.append(set_info)
+
+    json.dump(out, open('sets_raw.json', 'w'))
+
+    #print out
